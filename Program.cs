4e8c3580e422f44d83273cbac5ba6cfd55b3d265
guillegentile <guillugen@gmail.com>
2026-08-10 10:11:43 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
+string marketname = "open24";
+
+Console.WriteLine("ingrese su nombre: ");
+string username = Console.ReadLine();
+Console.WriteLine($"hola, {username} bienvenido a {marketname}");
+Console.ReadLine();
+
