0be07e242ca7bd083da4c92f1603ec8c4504ada8
guillegentile <guillugen@gmail.com>
2026-08-11 08:50:16 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ string username = Console.ReadLine();
 Console.WriteLine($"hola, {username} bienvenido a {marketname}");
 Console.ReadLine();
 
+Console.WriteLine("Ingrese el nombre del producto: ");
+string productname = Console.ReadLine();
+
+Console.WriteLine("Ingrese el precio del producto: ");
+decimal precio = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"El precio del producto es: {precio}");
+Console.ReadLine();
